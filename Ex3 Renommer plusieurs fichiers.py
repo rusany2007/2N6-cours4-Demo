abbd001c3b30121dbeb7c785bbbc840c41e29f07
rusany2007 <6302697@cegepmontpetit.ca>
2025-02-04 13:14:45 -0500

    ajout du cours 3

---
+++ Ex3 Renommer plusieurs fichiers.py
@@ -0,0 +1,31 @@
+import os
+# # allez dans le dossier Ex3 Videos
+info_env = os.environ
+user = info_env.get("USERPROFILE")
+(f"{user}/Documents")
+os.chdir("Ex3 videos")
+
+# # avec la boucle for, passez à travers chacun des dossiers de os.listdir()
+for list in os.listdir():
+    print(list)
+# #     utilisez os.path.splitext pour obtenir le filename et l'extension
+    print(os.path.splitext(f"{user}/Ex3 Videos"))
+
+# #     utilisez split sur le filename pour obtenir le titre, le cours et le numéro du cours¸
+
+
+for lit in os.listdir():
+    titre,cours,numero = lit.split("_")
+    print(titre,cours,numero)
+
+# #     utilisez strip pour enlever les espaces qui pourraient rester pour le titre et le numéro
+# #     en plus utilisez zfill pour remplir le numéro de sorte que 1 deviendra 01
+# #     recréez le nouveau nom de fichier#   utliser os.rename pour renommer le fichier
+
+for lit in os.listdir():
+    titre,cours,numero = lit.split("_")
+    print(titre.strip(),numero.strip())
+    print(numero.zfill(7))
+
+
+
